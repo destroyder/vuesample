17e02af0b5934558cc2472ffc85fb694ecbd78f7
destroyder <destroyder_ryo@yahoo.co.jp>
2024-01-26 11:28:19 +0900

    未ログインの時は、ログインページへリダイレクトする

--- src/utils/redirectLogout.ts
+++ src/utils/redirectLogout.ts
@@ -1,10 +1,12 @@
 export default function redirectLogout() {
   const redirectUrl = "/#/login";
   // localstrageを削除
-  // localStorage.removeItem("bearer");
+  localStorage.removeItem("bearer");
 
-  // alert('ログイン情報登録の未確認、もしくはログイン情報の有効期限が切れております。\n恐れ入りますが再度ログインをお願いいたします。');
-  // setTimeout(() => {
-  //     window.location.href = redirectUrl;
-  // }, 300);
+  alert(
+    "ログイン情報登録の未確認、もしくはログイン情報の有効期限が切れております。\n恐れ入りますが再度ログインをお願いいたします。"
+  );
+  setTimeout(() => {
+    window.location.href = redirectUrl;
+  }, 300);
 }
